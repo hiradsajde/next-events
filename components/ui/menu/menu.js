d986b5088f3366cc18fd4aee164b4563a076704d
hiradsajde <hirads.realy@gmail.com>
2021-11-22 14:44:46 +0330

    Inital commit

--- components/ui/menu/menu.js
+++ components/ui/menu/menu.js
@@ -3,15 +3,10 @@ import Link from 'next/link'
 const Menu = () => {
     return (
         <div className={style.menu}>
-            <div className={style.items}>
-                <ul>
-                    <Link href='/'><li>خانه</li></Link>
-                </ul>
-            </div>
             <div className={style.logo}>
                 <Link href='/'><img src="/logo.png"/></Link>
             </div>
-            <div className={style.button}>پنل کاربری</div>
+            <Link href="/favorites"><div className={style.button}>علاقه مندی ها</div></Link>
         </div>
     )
 }
